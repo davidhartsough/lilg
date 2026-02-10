475ea4e4821882c35272062ee75aedc437624ec4
David Hartsough <hartsoughdavid@gmail.com>
2026-02-10 02:16:50 -0700

    improve getMail and getEvents

--- src/lib/google.ts
+++ src/lib/google.ts
@@ -14,6 +14,19 @@ export interface CalEvent {
   start: string;
   end: string;
 }
+interface GCalEventData {
+  id: string;
+  summary: string;
+  description: string;
+  location: string;
+  status: string;
+  start: {
+    dateTime: string;
+  };
+  end: {
+    dateTime: string;
+  };
+}
 
 export async function getEvents(): Promise<CalEvent[]> {
   const token = await getAccessToken();
@@ -37,11 +50,11 @@ export async function getEvents(): Promise<CalEvent[]> {
     return [];
   }
   const data = await res.json();
-  const items = data.items ?? [];
+  const items: GCalEventData[] = data.items ?? [];
   const events: CalEvent[] = items
-    .filter((e: any) => e.status !== "cancelled" && !!e.start.dateTime)
+    .filter((e) => e.status !== "cancelled" && !!e.start.dateTime)
     .map(
-      (e: any): CalEvent => ({
+      (e): CalEvent => ({
         id: e.id,
         title: e.summary ?? "(No title)",
         description: e.description ?? "",
@@ -53,6 +66,7 @@ export async function getEvents(): Promise<CalEvent[]> {
   return events;
 }
 
+/*
 // snippet: string;
 // internalDate: string;
 // messages: {id threadId};
@@ -64,14 +78,47 @@ interface Message {
   id: string;
   threadId: string;
 }
+*/
 interface Thread {
   id: string;
   snippet: string;
 }
+interface MessageData {
+  id: string;
+  snippet: string;
+  internalDate: string;
+  payload: {
+    headers: [
+      {
+        name: "From";
+        value: string;
+      },
+      {
+        name: "Subject";
+        value: string;
+      },
+    ];
+  };
+}
+interface ThreadData {
+  id: string;
+  messages: MessageData[];
+}
+interface Message {
+  id: string;
+  snippet: string;
+  date: number;
+  from: string;
+}
+interface GmailConvo {
+  id: string;
+  subject: string;
+  messages: Message[];
+}
 
 const gmailUrl = "https://gmail.googleapis.com/gmail/v1/users/me/";
 const threadsUrl = `${gmailUrl}threads`;
-const messagesUrl = `${gmailUrl}messages`;
+// const messagesUrl = `${gmailUrl}messages`;
 const params = new URLSearchParams({
   maxResults: "40",
   labelIds: "INBOX",
@@ -90,6 +137,7 @@ async function getThreads(headers: RequestInit): Promise<Thread[]> {
   return data.threads ?? [];
 }
 
+/*
 // messages[] each = id and threadId
 async function getMessages(headers: RequestInit): Promise<Message[]> {
   const res = await fetch(`${messagesUrl}?${params}`, headers);
@@ -102,21 +150,23 @@ async function getMessages(headers: RequestInit): Promise<Message[]> {
   console.log("messages data:", data);
   return data.messages ?? [];
 }
+*/
 
 const metadataParams = new URLSearchParams({
   format: "metadata",
   metadataHeaders: "Subject",
 });
 metadataParams.append("metadataHeaders", "From");
-metadataParams.append("metadataHeaders", "Date");
-const metadataQuery = metadataParams.toString();
+// const metadataQuery = metadataParams.toString();
+metadataParams.append(
+  "fields",
+  "id,messages(id,snippet,internalDate,payload/headers)",
+);
+const threadQuery = metadataParams.toString();
 
 // email message IDs and email headers
 async function getThread(threadId: string, headers: RequestInit) {
-  const res = await fetch(
-    `${threadsUrl}/${threadId}?${metadataQuery}`,
-    headers,
-  );
+  const res = await fetch(`${threadsUrl}/${threadId}?${threadQuery}`, headers);
   if (!res.ok) {
     console.error("Failed to fetch thread", res.status);
     return [];
@@ -124,6 +174,7 @@ async function getThread(threadId: string, headers: RequestInit) {
   const data = await res.json();
   return data;
 }
+/*
 async function getMessage(messageId: string, headers: RequestInit) {
   const res = await fetch(
     `${messagesUrl}/${messageId}?${metadataQuery}`,
@@ -136,6 +187,7 @@ async function getMessage(messageId: string, headers: RequestInit) {
   const data = await res.json();
   return data;
 }
+*/
 
 export async function getMail() {
   console.time("getMail");
@@ -144,15 +196,34 @@ export async function getMail() {
   console.timeLog("getMail", "got token");
   const threads = await getThreads(headers);
   console.timeLog("getMail", "got threads");
-  const messages = await getMessages(headers);
-  console.timeLog("getMail", "got messages");
-  const threadPromises = threads.map(({ id }) => getThread(id, headers));
-  const threadsData = await Promise.all(threadPromises);
+  // const messages = await getMessages(headers);
+  // console.timeLog("getMail", "got messages");
+  const threadsData = await Promise.all(
+    threads.map(({ id }) => getThread(id, headers)),
+  );
   console.timeLog("getMail", "got thread data");
-  const messagePromises = messages.map(({ id }) => getMessage(id, headers));
-  const messagesData = await Promise.all(messagePromises);
-  console.timeLog("getMail", "got message data");
-  const gmailData = { threads: threadsData, messages: messagesData };
+  // const messagePromises = messages.map(({ id }) => getMessage(id, headers));
+  // const messagesData = await Promise.all(messagePromises);
+  // console.timeLog("getMail", "got message data");
+  // const gmailData = { threads: threadsData, messages: messagesData };
+  const gmailData: GmailConvo[] = threadsData.map((thread: ThreadData) => ({
+    id: thread.id,
+    snippet:
+      threads.find(({ id }) => id === thread.id)?.snippet ??
+      thread.messages[-1]?.snippet ??
+      "(No snippet)",
+    subject:
+      thread.messages[0]?.payload.headers.find((h) => h.name === "Subject")
+        ?.value ?? "(Unknown subject)",
+    messages: thread.messages.map((msg) => ({
+      id: msg.id,
+      snippet: msg.snippet,
+      date: Number(msg.internalDate),
+      from:
+        msg.payload.headers.find((h) => h.name === "From")?.value ??
+        "(Unknown sender)",
+    })),
+  }));
   console.timeEnd("getMail");
   return gmailData;
 }
