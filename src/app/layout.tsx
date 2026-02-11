47eb6463beba894ec246bf13eb2e772a35917b1c
David Hartsough <hartsoughdavid@gmail.com>
2026-02-10 22:28:21 -0700

    more improved styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Lil' G",
+  title: "lil' g",
   description:
     "Super simple, minimal Google app info from Gmail and Google Calendar.",
 };
