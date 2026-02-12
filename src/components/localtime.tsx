c3cf6f52867d16679b73941cf0442aeaeb92f406
David Hartsough <hartsoughdavid@gmail.com>
2026-02-11 21:37:53 -0700

    format locale date time string

--- src/components/localtime.tsx
+++ src/components/localtime.tsx
@@ -7,7 +7,17 @@ export default function LocalTime({ datetime }: { datetime: string | number }) {
   const [ISOString, setISOString] = useState("");
   useEffect(() => {
     const date = new Date(datetime);
-    setLocalTime(date.toLocaleString());
+    setLocalTime(
+      date.toLocaleString(undefined, {
+        weekday: "short",
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        dayPeriod: "short",
+      }),
+    );
     setISOString(date.toISOString());
   }, [datetime]);
   return localTime ? <time dateTime={ISOString}>{localTime}</time> : null;
